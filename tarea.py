278d3ca2eb57903f518d5a631c9c255b00190e65
erenyt12 <erenyt12@gmail.com>
2021-08-19 17:57:28 -0300

    mi lista

--- tarea.py
+++ tarea.py
@@ -12,3 +12,31 @@ tuple = (ciudad_1, ciudad_2, ciudad_3, ciudad_4)
 #print(tuple)
 
 ciudad_1, ciudad_2, ciudad_3, ciudad_4 = tuple
+
+print ("se mostratan su primera ciudad ingresada y la ultima: ")
+print(ciudad_1,  ciudad_4)
+
+datos_de_su_circulo = float(input("ingrese el radio de su circulo para calcular el area: "))
+
+area = datos_de_su_circulo * 3.14
+
+resultado_de_area = round(area,2)
+
+print("su area es: ",resultado_de_area)
+
+mi_lista.insert(0, input("ingrese  el primer numero: "))
+
+mi_lista.insert(1, input("ingrese  el segundo numero: "))
+
+mi_lista.insert(2, input("ingrese  el tercer numero: "))
+
+mi_lista.insert(3, input("ingrese  el cuarto numero: "))
+
+mi_lista.insert(4, input("ingrese  el quinto numero: "))
+
+mi_lista.insert(5, input("ingrese  el sexto numero: "))
+
+mi_lista.insert(6, input("ingrese  el septimo numero: "))
+
+for i in mi_lista:
+    print(i)
