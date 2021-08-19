f4972594940ac091aece36b4a6921cb02aee7fe2
erenyt12 <erenyt12@gmail.com>
2021-08-19 17:46:55 -0300

    empezando

---
+++ tarea.py
@@ -0,0 +1,14 @@
+mi_lista = []
+
+#print(type(mi_lista))
+
+ciudad_1= input("ingrese la primer ciudad: ")
+ciudad_2 = input("ingrese la segunda ciudad: ")
+ciudad_3 = input("ingrese la tercer ciudad: ")
+ciudad_4 = input("ingrese la cuarta ciudad: ")
+
+tuple = (ciudad_1, ciudad_2, ciudad_3, ciudad_4)
+
+#print(tuple)
+
+ciudad_1, ciudad_2, ciudad_3, ciudad_4 = tuple
