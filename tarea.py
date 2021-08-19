6c0ab92d4a1475259fbfd49a4b5599e711333e79
erenyt12 <erenyt12@gmail.com>
2021-08-19 18:00:28 -0300

    final

--- tarea.py
+++ tarea.py
@@ -40,3 +40,52 @@ mi_lista.insert(6, input("ingrese  el septimo numero: "))
 
 for i in mi_lista:
     print(i)
+
+dict_1 = {input("nombre de usuario: "): input("dni: "), input("nombre de usuario: "): input("dni: "), input("nombre de usuario: "): input("dni: "), input("nombre de usuario: "): input("dni: "), input("nombre de usuario: "): input("dni: ")} 
+
+#print(dict_1)
+
+print('''Menú
+     1- Mostrar los números de la lista
+     2- Mostrar ciudades    
+     3- Calcular el área de un círculo
+     4- Agregar número a la lista y        mostrar los números distintos
+     5- Agregar usuario y DNI 
+     6- Buscar usuario por nombre e imprimir su DNI''')
+
+   
+
+continuar = True
+while continuar:
+    opcion = input("elija una opcion: ")
+    
+    if opcion == '1':
+        for i in mi_lista:
+             print(i)
+    elif opcion == '2':
+        print(ciudad_1, ciudad_2, ciudad_3, ciudad_4)
+    elif opcion == '3':
+        datos_de_su_circulo = float(input("ingrese el radio de su circulo para calcular el area: "))
+        area = datos_de_su_circulo * 3.14
+        resultado_de_area = round(area,2)
+        print("su area es: ",resultado_de_area)
+    elif opcion == '4':
+        mi_lista.append(input("ingrese nuevo numero a la lista: "))
+        nuevo_numero = mi_lista[-1]
+        print("el nuevo numero agregado es: ", nuevo_numero)
+    elif opcion == '5':
+            dict_1[input("ingresar usuario: ")] = input("ingresar dni: ")
+            #print(dict_1)
+    elif opcion == '6':
+            buscar_usuario = input("ingrese el usuario que quiere buscar: ")
+            if buscar_usuario in dict_1:
+                dni = dict_1[buscar_usuario]
+                print("su DNI es: ",dni)
+            else:
+                print("este usuario no se encuentra en el sistema, intente nuevamente ")
+                buscar_usuario = input("ingrese el usuario que quiere buscar: ")
+            
+    else:
+        print("opcion incorrecta, saliendo del sistema...\n")
+
+        continuar = False
